2bfb6962a114ce53fe9971c20122666fa595928f
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-01-19 19:45:07 -0300

    reorganization

--- desafio_1.mk
+++ desafio_1.mk
@@ -4,17 +4,17 @@ Usuário inserir uma DATA
 Salvar a DATA do Usuário
 
 Dividendo = quociente * divisor + resto
-2022 = (20) * 100 + 22
+ex.: 2022 = (20) * 100 + 22
 
 D = Dividendo
 d = divisor
 q = quociente
 r = resto
 
-Se D < 100, então século 1
+Se D <= 100, então século 1
 
 Se D>100, então faça:
 	D = q * d + r
 
-Se r = 0, então o século é q
+Se r igual a 0, então o século é q
 Se r diferente de zero, então o século é q + 1
