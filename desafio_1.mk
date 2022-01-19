d539530f91fd58e111a8c264466df47506fe1fc2
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-01-19 18:39:46 -0300

    concept

---
+++ desafio_1.mk
@@ -0,0 +1,19 @@
+DESAFIO 1
+
+Usuário inserir uma DATA
+Salvar a DATA do Usuário
+
+Dividendo = quociente * divisor + resto
+2022 = (20) * 100 + 22
+
+D -> Ano que a pessoa digita
+
+D = 100d + r
+
+Se D < 100, então século 1
+Se D>100, então faça:
+
+D = 100d + r
+
+Se r = 0, então o século é d.
+Se r diferente de zero, então o século é d+1
