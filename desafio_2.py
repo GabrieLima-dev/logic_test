24504d80f43606d4716ed9d66b01a87a61f23960
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-01-19 21:48:07 -0300

    Desafio 2

---
+++ desafio_2.py
@@ -0,0 +1,5 @@
+print("Olá, nosso jogo irá começar.\n Teremos 10 partidas.\n Que vença o melhor time!")
+
+# Partida 1
+x1 = int(input("Digite o número de gols do Time 1 nesta partida.\n OBS: Apenas digite números de 0 a 4\n "))
+x2 = int(input("Digite o número de gols do Time 2 nesta partida.\n OBS: Apenas digite números de 0 a 4\n "))
