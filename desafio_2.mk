d9a05d424b028cc43cc89bf16c271c7738d3e94b
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-01-19 21:46:34 -0300

    concept

---
+++ desafio_2.mk
@@ -0,0 +1,23 @@
+Desafio 2
+
+Pedir ao usuário que digite o número de gols das 10 partidas dos 2 times.
+Partida 1
+x = Time 1: 0 a 4
+y = Time 2: 0 a 4
+
+Validação: 
+Pontuação de 0 a 4 apenas. (while)
+
+Em cada partida Pontuação:
+se x > y - 3 pontos
+se x < y - 0 ponto
+se x = y - 1 ponto
+e imprimir a pontuação
+
+...
+
+Partida 10
+Time 1:
+Time 2:
+
+Final das partidas somar as pontuações de cada time.
