6dac23af102260a802e89e2512b6cff03f8e9637
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-01-24 20:38:55 -0300

    challenge completed

--- desafio_2.py
+++ desafio_2.py
@@ -1,16 +1,32 @@
+temp = []
+princ = []
+
+
 def partida():
-    lista = [0, 1, 2, 3, 4]
+    list = [0, 1, 2, 3, 4]
     x = ""
     y = ""
-    while x not in lista:
+    while x not in list:
         x = int(input(
             "Digite o número de gols do Time 1 nesta partida.\n OBS: Apenas digite números de 0 a 4   "))
 
-    while y not in lista:
+    while y not in list:
         y = int(input(
             "Digite o número de gols do Time 2 nesta partida.\n OBS: Apenas digite números de 0 a 4   "))
 
+    print()
     print("O resultado desta partida foi {}:{}".format(x, y))
+    pontuacao(x, y)
+
+
+def pontuacao(a, b):
+    if a > b:
+        temp.append(3)
+    if a < b:
+        temp.append(0)
+    if a == b:
+        temp.append(1)
+    princ.append(temp[:])
 
 
 def n_partida():
@@ -26,3 +42,6 @@ def n_partida():
 print("Olá, nosso jogo irá começar.\n Teremos 10 partidas.\n Que vença o melhor time!")
 print()
 n_partida()
+print()
+print("O nosso ao fim do campeonato teve um total de {} pontos".format(
+    sum(princ[9])))
