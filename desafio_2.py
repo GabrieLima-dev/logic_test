0073844075f271c338341f482a53abb9ffd8d2e9
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-01-24 10:30:19 -0300

    validation

--- desafio_2.py
+++ desafio_2.py
@@ -1,5 +1,28 @@
-print("Olá, nosso jogo irá começar.\n Teremos 10 partidas.\n Que vença o melhor time!")
+def partida():
+    lista = [0, 1, 2, 3, 4]
+    x = ""
+    y = ""
+    while x not in lista:
+        x = int(input(
+            "Digite o número de gols do Time 1 nesta partida.\n OBS: Apenas digite números de 0 a 4   "))
 
-# Partida 1
-x1 = int(input("Digite o número de gols do Time 1 nesta partida.\n OBS: Apenas digite números de 0 a 4\n "))
-x2 = int(input("Digite o número de gols do Time 2 nesta partida.\n OBS: Apenas digite números de 0 a 4\n "))
+    while y not in lista:
+        y = int(input(
+            "Digite o número de gols do Time 2 nesta partida.\n OBS: Apenas digite números de 0 a 4   "))
+
+    print("O resultado desta partida foi {}:{}".format(x, y))
+
+
+def n_partida():
+    numeroPartida = 1
+    while numeroPartida <= 10:
+        print()
+        print('**** Partida', numeroPartida, '****')
+        print()
+        partida()
+        numeroPartida += 1
+
+
+print("Olá, nosso jogo irá começar.\n Teremos 10 partidas.\n Que vença o melhor time!")
+print()
+n_partida()
