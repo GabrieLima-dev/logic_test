f180b6bea665bfaefe3a964d7553676709ec677d
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-02-02 08:26:10 -0300

    Rename Challenge_1

---
+++ challenge_1.py
@@ -0,0 +1,17 @@
+date = int(input("Informe aqui uma data e lhe direi em qual século está: "))
+
+D = date
+d = 100
+q = int(date/d)
+r = date % d
+x = q + 1
+
+if D <= 100:
+    print("Com o ano {} estamos falando do século 1".format(D))
+
+if D > 100:
+    D = q * d + r
+    if r == 0:
+        print("Com o ano {} estamos falando do século {}".format(D, q))
+    else:
+        print("Com o ano {} estamos falando do século {}".format(D, x))
